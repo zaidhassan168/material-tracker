4653f142511abeb1b2962b93e229a452a2bce403
zaidhassan168 <zaidhassan168@outlook.com>
2025-04-01 09:04:21 +0500

    AAA

--- app/select-role.tsx
+++ app/select-role.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import RoleSelectionScreen from './components/auth/RoleSelectionScreen';
-
+//COMMENT
 const SelectRolePage = () => {
   return <RoleSelectionScreen />;
 };
